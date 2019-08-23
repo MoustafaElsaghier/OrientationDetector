2d78b1c5c23f3d79779f89326295f60f5869508e
EL_Saghier <thedreamereng@gmail.com>
2019-08-23 11:14:09 +0200

    getting the actual angel user hold phone

--- orientationdetector/src/main/java/app/elsaghier/com/orientationdetector/OrientationEvent/OrientationDetector.java
+++ orientationdetector/src/main/java/app/elsaghier/com/orientationdetector/OrientationEvent/OrientationDetector.java
@@ -6,8 +6,26 @@ import android.view.OrientationEventListener;
 public class OrientationDetector extends OrientationEventListener {
 
     private boolean isPortrait;
+
+
     private int rotateValue;
 
+    public int getActualRotateValue() {
+        return actualRotateValue;
+    }
+
+    /**
+     * return the actual angel user hold phone ( from 0 to 359 ).
+     */
+    private int actualRotateValue;
+
+    /**
+     * return the orientation value
+     * 0 for portrait
+     * 90 for landscape
+     * 180 for reverse-portrait
+     * 270 for reverse-landscape
+     */
     public int getRotateValue() {
         return rotateValue;
     }
@@ -21,7 +39,7 @@ public class OrientationDetector extends OrientationEventListener {
         if (orientation < 0) {
             return; // Flip screen, Not take account
         }
-
+        actualRotateValue = orientation;
         if (orientation <= 45) {
             isPortrait = true;
         } else if (orientation <= 135) {
